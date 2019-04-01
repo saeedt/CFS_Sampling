11d55e0d58d1d91f5dc3403339c668cd9fa3fa32
Saeed Tehrani <saeed.tehrani@gmail.com>
2019-04-01 17:27:52 -0400

    Added scripts, raw data, and final data files as well as the readme.

---
+++ SQL/Generate_est.sql
@@ -0,0 +1,50 @@
+/*generate_est is a function written in procedural PostgreSQL that generates certain number of establishments and distributes them in state, county, NAICS categories based on fafcbp table. fafcbp is the combination of FAF and CBP datasets. */
+/*Usage:
+generate_est(Sample_size, Source_table, Value_CV, Mile_CV)
+Example: 
+SELECT * FROM generate_est(100000, 'fafcbp, 0.5, 0.5);
+*/
+
+drop function if exists generate_est(integer,text,numeric,numeric);
+drop type if exists returntype;
+
+create type returntype as(
+state smallint,
+county smallint,
+naics char(4),
+estno smallint,
+value real,
+miles real);
+
+CREATE OR REPLACE FUNCTION generate_est(_psize int = 1000, _source text = 'fafcbp', _ValCV numeric = 0.5 , _MilCV numeric = 0.5)
+  RETURNS SETOF returntype AS
+$func$
+DECLARE
+	r returntype%rowtype;
+	_rec record;
+	_est_index int:=1;
+	_est_county int:=0;
+	_total_est int:=0;
+	_tbl_size int:=0; 
+	
+BEGIN
+	EXECUTE 'SELECT SUM(est) FROM '||$2 INTO _total_est;
+	EXECUTE 'SELECT COUNT(*) FROM '||$2 _tbl_size;
+	FOR _rec IN EXECUTE 'select * from ' || $2 ||' ORDER BY 1,2,3' LOOP
+		_est_county = round((_rec.est*_psize*1.0)/_total_est);
+		IF _est_county >=1 THEN
+			FOR _est_index IN 1.._est_county LOOP
+				r.estno = _est_index;
+				r.state = _rec.state;
+				r.county = _rec.county;
+				r.naics = _rec.naics;
+				r.value = abs(normal_rand(1,_rec.value, _rec.value*_ValCV));
+				r.miles = abs(normal_rand(1,_rec.miles, _rec.miles*_MilCV));
+				RETURN next r;
+			END LOOP;
+		END IF;			
+	END LOOP;		
+	RETURN;
+END
+$func$  
+LANGUAGE plpgsql;
---
+++ SQL/SQL_Scripts.sql
@@ -0,0 +1,67 @@
+//Create cbp table for county business pattern data
+create table cbp(
+state smallint, 
+county smallint, 
+naics char(6),
+empflag char(1),
+empnf char(1),
+emp int,
+qp1_nf char(1),
+qp1 int,
+ap_nf char(1),
+ap int,
+est int,
+n1_4 int,
+n5_9 int,
+n10_9 int,
+n20_49 int,
+n50_99 int,
+n100_249 int,
+n250_499 int,
+n500_999 int,
+n1000 int,
+n1000_1 int,
+n1000_2 int,
+n1000_3 int,
+n1000_4 int,
+censtate int,
+cencty int);
+
+//Create sctnaics table for SCTG NAICS mapping data
+create table sctgnaics(
+sctg char(2),
+context varchar(50),
+naics char(4));
+
+//Create faf16 table for FAF 2016 data
+create table faf16(
+orig char(3),
+dest char(3),
+sctg char(2),
+ton real,
+value real,
+tmile real,
+curval real, 
+wgtdist real);
+
+/* Aggregate value, weight, and mile by state and SCTG in faf16 tables
+Then replace sctg with naics based on the mapping defined in sctgnaics table
+store the results in faf16s table */
+with faf16s as (select substring(orig,1,2) as state, sctg, sum(ton) as weight, sum(value) as value, sum(tmile) as miles from faf16 group by 1,2)
+select state, naics, weight, value, miles into faf16s from faf16s inner join sctgnaics on faf16s.sctg = sctgnaics.sctg;
+
+/* Remove total rows from CBP data (NAICS codes with - or /)
+Aggregate NAICS code according to the 2017 CFS sample design spec(Table 1 - Pages 7 and 8)*/
+with cbps as (select state, county, naics, ap, est from cbp where naics not similar to '%(-|/)%'), 
+cbpt as ((select state, county, substring(naics,1,4) as naics, sum(ap) as ap, sum(est) as est from cbps where substring(naics,1,4) in ('2121','2122','2123','4231','4232','4233','4234','4235','4236','4237','4238',
+'4239','4241','4242','4243','4244','4245','4246','4247','4248','4249','4541','4543','5111') group by 1,2,3) union all (select state, county, substring(naics,1,3) as naics, sum(ap) as ap, sum(est) as est from cbps where substring(naics,1,3) in ('311','312','313','314','315','316','321','322','323','324','325','326','327','331',
+'332','333','334','335','336','337','339') group by 1,2,3));
+
+/* Create fafbp table with weight, value, miles, and total number of establishments per state/NAICS  */
+with sbp as (select state, naics, sum(ap) as ap, sum(est) as est from cbpf group by 1,2)
+select sbp.state, sbp.naics, weight, value, miles, ap, est into fafbp from faf16s inner join sbp on faf16s.naics = sbp.naics and faf16s.state::smallint = sbp.state;  
+
+/* Estimate weight, value, and miles for each county and storing the results in fafcbp table */
+select cbpf.state, cbpf.county, cbpf.naics, (cbpf.est*fafbp.weight)/fafbp.est as weight, (cbpf.est*fafbp.value)/fafbp.est as value, 
+(cbpf.est*fafbp.miles)/fafbp.est as miles, cbpf.ap as ap, cbpf.est as est into fafcbp from cbpf 
+inner join fafbp on cbpf.state = fafbp.state and fafbp.naics = cbpf.naics;
